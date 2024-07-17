c7d0e1c69f72f4b21d57343836466e199685f32a
ShreeRajput <156456032+ShreeRajput@users.noreply.github.com>
2024-07-17 19:19:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,49 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import authRouter from './routes/auth.js'
-import userRouter from './routes/userRoutes.js'
-import postRouter from './routes/postRoutes.js'
+import express from 'express';
+import mongoose from 'mongoose';
+import authRouter from './routes/auth.js';
+import userRouter from './routes/userRoutes.js';
+import postRouter from './routes/postRoutes.js';
+import path from 'path';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import conversationRouter from './routes/conversationRoutes.js'
-import messageRouter from './routes/messageRoutes.js'
+import conversationRouter from './routes/conversationRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
 
 dotenv.config();
-const app = express()
-const port = process.env.PORT || 8000
+const app = express();
+const port = process.env.PORT || 8000;
 
-// Enable CORS middleware for any origin
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-}))
+}));
 
-mongoose.connect(process.env.MONGO_URL)
-    .then( ()=>{
-        app.listen(port , ()=> {
-            console.log("server is active on "+ port)
-            console.log("connected to mongodb")
-        } )
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(express.json());
+
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/posts', postRouter);
+app.use('/api/conversation', conversationRouter);
+app.use('/api/message', messageRouter);
+
+app.get('/', (req, res) => {
+    res.send("Hello, this is the root API");
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(port, () => {
+            console.log("Server is active on port " + port);
+            console.log("Connected to MongoDB");
+        });
     })
-    .catch((error)=>{
-        console.log(error)
-        console.log("not able to connect with db")
-    })
-
-//middlewares
-
-app.use(express.json())
-
-app.use('/api/users',userRouter)
-app.use('/api/auth', authRouter)
-app.use('/api/posts',postRouter)
-app.use('/api/conversation',conversationRouter)
-app.use('/api/message',messageRouter)
-
-app.get('/',(req,res)=>{
-    res.send("Hello this root api")
-})
+    .catch((error) => {
+        console.error("Failed to connect to MongoDB", error);
+    });
